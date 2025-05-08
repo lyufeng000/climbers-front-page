dd322bc0369ac5929922aaf8bf483853984ff6dc
Feng <lyufeng000@qq.com>
2025-05-08 17:52:48 +0800

    增加了新页面们的路由表

--- src/router/index.js
+++ src/router/index.js
@@ -1,39 +1,62 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/guest/HomeView.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/member',
-    name: 'member',
-    component: () => import('../views/MemberView.vue')
-  },
-  {
-    path: '/contact',
-    name: 'contact',
-    component: () => import('../views/ContactView.vue')
-  },
-  {
-    path: '/about',
-    name: 'about',
-    component: () => import('../views/AboutView.vue')
-  },
-  {
-   path: '/logIn',
-   name: 'logIn',
-    component: () => import('../views/LogInView.vue')
-  }
+    {
+        //游客主页面
+        path: '/',
+        name: 'home',
+        component: HomeView
+    },
+    {
+        //游客成员信息
+        path: '/guest/member',
+        name: 'guestMember',
+        component: () => import('../views/guest/MemberView.vue')
+    },
+    {
+        //游客联系我们
+        path: '/contact',
+        name: 'contact',
+        component: () => import('../views/ContactView.vue')
+    },
+    {
+        //关于我们
+        path: '/about',
+        name: 'about',
+        component: () => import('../views/AboutView.vue')
+    },
+    {
+        //登陆页面
+        path: '/logIn',
+        name: 'logIn',
+        component: () => import('../views/guest/LogInView.vue')
+    },
+    {
+        //管理员主页面
+        path: '/admin/home',
+        name: 'adminHome',
+        component: () => import('../views/admin/HomeView.vue')
+    },
+    {
+        //管理员成员管理页面
+        path: '/admin/member',
+        name: 'generateMembers',
+        component: () => import('../views/admin/GenerateView.vue')
+    },
+    {
+        //成员主页面
+        path: '/member/home',
+        name: 'memberHome',
+        component: () => import('../views/member/HomeView.vue')
+    }
 ]
 
 const router = new VueRouter({
-  routes
+    routes
 })
 
 export default router
