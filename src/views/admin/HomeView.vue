e4eb87a81446a2bb7acbc92416a0789e5f07759d
Feng <lyufeng000@qq.com>
2025-05-08 17:51:15 +0800

    管理员主页面

---
+++ src/views/admin/HomeView.vue
@@ -0,0 +1,31 @@
+<!--管理员主页面-->
+<template>
+  <el-container>
+    <el-header>
+      <el-container>
+        <el-aside>
+          <el-image :src="logo"></el-image>
+        </el-aside>
+        <el-main>
+          <el-menu mode="horizontal">
+            <el-menu-item index="1" @click="$router.push('/admin/member')">成员管理</el-menu-item>
+          </el-menu>
+        </el-main>
+      </el-container>
+    </el-header>
+
+
+    <!--主展示区-->
+    <el-main>main</el-main>
+  </el-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      logo: require('@/assets/logo.jpg')
+    }
+  }
+}
+</script>
