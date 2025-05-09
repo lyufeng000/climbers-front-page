a7c1f81f0b8aad8e98df5bea1a065818ba47489c
lyufeng000 <1191058133@qq.com>
2025-05-09 15:07:52 +0800

    端口监听为全域，本机端口8080

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,15 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer:{
-    proxy:{
-        '/api': {
-            target: 'http://localhost:8080', // 确保后端服务地址正确
-            changeOrigin: true,
-            pathRewrite: { '^/api': '' } // 确保路径重写规则正确
+    transpileDependencies: true,
+    devServer: {
+        port: 8000, // 确保端口号正确
+        host: '0.0.0.0', // 确保主机地址正确
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080', // 确保后端服务地址正确
+                changeOrigin: true,
+                pathRewrite: {'^/api': ''} // 确保路径重写规则正确
+            }
         }
     }
-  }
 })
