e285330d265911e03d1bc0e3dffff2a94392d1e7
lyufeng000 <1191058133@qq.com>
2025-05-09 15:06:40 +0800

    管理员主页面，使管理栏在主页面可见

--- src/views/admin/HomeView.vue
+++ src/views/admin/HomeView.vue
@@ -2,30 +2,54 @@
 <template>
   <el-container>
     <el-header>
-      <el-container>
-        <el-aside>
-          <el-image :src="logo"></el-image>
-        </el-aside>
-        <el-main>
-          <el-menu mode="horizontal">
-            <el-menu-item index="1" @click="$router.push('/admin/member')">成员管理</el-menu-item>
-          </el-menu>
-        </el-main>
-      </el-container>
+      <el-image :src="logo" class="img-logo" />
     </el-header>
-
-
-    <!--主展示区-->
-    <el-main>main</el-main>
+    <el-container>
+      <el-aside width="200px">
+        <el-menu @select="handleMenuClick">
+          <el-menu-item index="memberView">成员信息</el-menu-item>
+          <el-menu-item index="manageView">成员管理</el-menu-item>
+          <!-- 👉 你可以继续添加页面组件入口 -->
+          <!-- <el-menu-item index="UserView">用户管理</el-menu-item> -->
+        </el-menu>
+      </el-aside>
+      
+      <el-main>
+        <!-- 动态组件渲染 -->
+        <component :is="currentView" />
+      </el-main>
+    </el-container>
   </el-container>
 </template>
 
 <script>
+import ManageView from '@/views/admin/ManageView.vue'
+import MemberView from "@/views/member/MemberView.vue";
+// import UserView from '@/views/admin/UserView.vue'  // 后续你可以添加更多组件
+
 export default {
+  components: {
+    manageView: ManageView,
+    memberView: MemberView,
+    // UserView
+  },
   data() {
     return {
-      logo: require('@/assets/logo.jpg')
+      logo: require('@/assets/logo.jpg'),
+      currentView: 'manageView'  // 默认显示的组件名称（和组件名一致）
+    };
+  },
+  methods: {
+    handleMenuClick(index) {
+      this.currentView = index
     }
   }
 }
 </script>
+
+<style>
+.img-logo {
+  width: 200px;
+  height: auto;
+}
+</style>
