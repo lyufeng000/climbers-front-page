cb93ca52f76bc15a870ed4ad0b3f8e4870e461ad
lyufeng000 <1191058133@qq.com>
2025-05-09 15:07:29 +0800

    团队成员的列表

---
+++ src/views/member/MemberView.vue
@@ -0,0 +1,17 @@
+<!--团队队员可以看见的成员列表-->
+<template>
+  <el-container>
+   <el-header>
+     <div align="center" style="font-size: 30px">成员列表</div>
+   </el-header>
+    
+    
+    <el-main>
+      main
+    </el-main>
+  </el-container>
+</template>
+    
+<script>
+    
+</script>
